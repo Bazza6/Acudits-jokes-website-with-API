25d60042ccc83a39a5f90c08a214578979665951
Nazareno Valentini <nazareno.valentini@gmail.com>
2022-07-13 12:11:26 +0200

    Ex. 4:  display meteo from API

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let acudit = "";
 
 const getJoke = async () => {
     document.querySelectorAll(".puntuacionButtons").forEach(a => a.style.display = "inline");
+
     try {
         const respuesta = await fetch("https://icanhazdadjoke.com/", {
             headers: {
@@ -23,7 +24,34 @@ const getJoke = async () => {
     } catch (error) {
         console.log(error);
     }
+
 }
+
+const getMeteo = async () => { // b8bbbf0fe403ed96607907fcfdeb0295
+
+    try {
+        const respuesta = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=41.3828939&lon=2.1774322&appid=b8bbbf0fe403ed96607907fcfdeb0295&lang=es");
+        console.log("respuesta", respuesta);
+        const datos = await respuesta.json(); //a que sirve .json() ?
+
+        console.log("meteo datos: ", datos);
+        meteo = datos.weather[0].description;
+        console.log("meteo Barcelona: ", meteo);
+
+        /*         meteo = datos.joke;
+                document.getElementById("acudit").innerHTML = `<cite>"${acudit}"</cite>`;
+        
+                //console.log("datos (respuesta.json()): ", datos);
+                console.log("acudit: ", acudit);
+            } */
+    } catch (error) {
+        console.log(error);
+    }
+    document.getElementById("meteo").innerHTML = `Avui en Barcelona: ${meteo}`;
+
+}
+
+getMeteo();
 
 function puntuacion(score) {
     let report = {
