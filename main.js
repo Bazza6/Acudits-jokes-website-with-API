53c447b386ee9ec778b30c79056e6f772195a55f
Nazareno Valentini <nazareno.valentini@gmail.com>
2022-07-17 11:23:25 +0200

    clean code and add some icon

--- main.js
+++ main.js
@@ -1,98 +1,100 @@
 const reportAcudits = []
 let acudit = "";
 
-function getJoke() {
-    random1 = Math.floor(Math.random() * 6);
-    random2 = Math.floor(Math.random() * 6);
-    random3 = Math.floor(Math.random() * 6);
-    random4 = Math.floor(Math.random() * 6);
-    document.body.style.backgroundImage = `url(./blob/blob${random1}.svg)`;
-    document.getElementById("blobSx").style.backgroundImage = `url(./blob/blob${random2}.svg)`;
-    document.getElementById("blobDx").style.backgroundImage = `url(./blob/blob${random3}.svg)`;
-    if (random4 >= 3) { //acudit casual entre las dos APIs
+const showButtons = () => {  //show puntuaction buttons
+    let negativeButton = document.querySelector(".negativeButton");
+    let neutralButton = document.querySelector(".neutralButton");
+    let positiveButton = document.querySelector(".positiveButton");
+    negativeButton.style.display = "inline";
+    neutralButton.style.display = "inline";
+    positiveButton.style.display = "inline";
+}
+
+const hideButtons = () => { // hide puntuaction buttton
+    let negativeButton = document.querySelector(".negativeButton");
+    let neutralButton = document.querySelector(".neutralButton");
+    let positiveButton = document.querySelector(".positiveButton");
+    negativeButton.style.display = "none";
+    neutralButton.style.display = "none";
+    positiveButton.style.display = "none";
+}
+
+const getJoke = () => {
+    let random1 = Math.floor(Math.random() * 6);
+    let random2 = Math.floor(Math.random() * 6);
+    let random3 = Math.floor(Math.random() * 6);
+    let random4 = Math.floor(Math.random() * 6);
+    let blobCentro = document.body;
+    let blobSx = document.getElementById("blobSx");
+    let blobDx = document.getElementById("blobDx");
+    blobCentro.style.backgroundImage = `url(./blob/blob${random1}.svg)`;
+    blobSx.style.backgroundImage = `url(./blob/blob${random2}.svg)`;
+    blobDx.style.backgroundImage = `url(./blob/blob${random3}.svg)`;
+    if (random4 >= 3) {  // random joke between dad jokes and chuck jokes
         getAcudit()
     } else {
         getChuck()
     }
 }
 
-const getAcudit = async () => {
-    document.querySelectorAll(".negativeButton").forEach(a => a.style.display = "inline");
-    document.querySelectorAll(".neutralButton").forEach(a => a.style.display = "inline");
-    document.querySelectorAll(".positiveButton").forEach(a => a.style.display = "inline");
-
+const getAcudit = async () => { // dad jokes
     try {
-        const respuesta = await fetch("https://icanhazdadjoke.com/", {
+        let respuesta = await fetch("https://icanhazdadjoke.com/", {
             headers: {
                 'Accept': 'application/json'
             }
-        });  //await sirve a decirle que acabe la peticion antes de pasar 
-        // a la linea siguiente. Se puede usar solo con funciones asincronas
-        //console.log("respuesta: ", respuesta);
-
-        const datos = await respuesta.json();
+        });
+        let datos = await respuesta.json();
         acudit = datos.joke;
-        document.getElementById("acudit").innerHTML = `<cite>"${acudit}"</cite>`;
-
-        //console.log("datos (respuesta.json()): ", datos);
-        console.log("acudit: ", acudit);
-        //console.log("JSON.stringify(datos): ", JSON.stringify(datos));
-
+        let acuditHTML = document.getElementById("acudit");
+        acuditHTML.innerHTML = `<cite>"${acudit}"</cite>`;
+        showButtons();
     } catch (error) {
         console.log(error);
     }
-
 }
-const getChuck = async () => {
-    document.querySelectorAll(".negativeButton").forEach(a => a.style.display = "inline");
-    document.querySelectorAll(".neutralButton").forEach(a => a.style.display = "inline");
-    document.querySelectorAll(".positiveButton").forEach(a => a.style.display = "inline");
 
+const getChuck = async () => { // chuck norris jokes
     try {
-        const respuesta = await fetch("https://api.chucknorris.io/jokes/random");  //await sirve a decirle que acabe la peticion antes de pasar 
-        // a la linea siguiente. Se puede usar solo con funciones asincronas
-        console.log("respuesta2: ", respuesta);
-
-        const datos = await respuesta.json();
+        let respuesta = await fetch("https://api.chucknorris.io/jokes/random");
+        let datos = await respuesta.json();
         acudit = datos.value;
-        document.getElementById("acudit").innerHTML = `<cite>"${acudit}"</cite>`;
-
+        acuditHTML = document.getElementById("acudit");
+        acuditHTML.innerHTML = `<cite>"${acudit}"</cite>`;
+        showButtons();
     } catch (error) {
         console.log(error);
     }
-
 }
+
 const getMeteo = async () => {
-
-    try { //units=metric for temperature in c. 
-        const respuesta = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=41.3828939&lon=2.1774322&appid=b8bbbf0fe403ed96607907fcfdeb0295&lang=es&units=metric");
-        console.log("respuesta", respuesta);
+    try {
+        const apiKey = "b8bbbf0fe403ed96607907fcfdeb0295"; // pleas don't steal my API key!!!
+        const apiLang = "es";
+        const apiUnit = "metric";
+        const apiLatLon = "lat=41.3828939&lon=2.1774322";  // Barcelona latitude & longitude
+        const respuesta = await fetch(`https://api.openweathermap.org/data/2.5/weather?${apiLatLon}&appid=${apiKey}&lang=${apiLang}&units=${apiUnit}`);
         const datos = await respuesta.json();
-
-        console.log("meteo datos: ", datos);
         meteoTemp = Math.round(datos.main.temp);
         meteoIcono = datos.weather[0].icon;
-        console.log("meteo Barcelona: ", meteo);
-        console.log("meteo icono: ", meteoIcono);
-
     } catch (error) {
         console.log(error);
     }
-    document.getElementById("meteo").innerHTML = `${meteoTemp} ºC`;
-    document.getElementById("icono").src = `./icon/${meteoIcono}.png`;
+    let temperatura = document.getElementById("meteo");
+    temperatura.innerHTML = `${meteoTemp} ºC`;
+    let icono = document.getElementById("icono");
+    icono.src = `./icon/${meteoIcono}.png`;
 }
 
-getMeteo();
-
-function puntuacion(score) {
+const puntuacion = score => {
     let report = {
         joke: acudit,
         score: score,
         date: (new Date).toISOString(),
     }
-    reportAcudits.push(report);
-    document.querySelectorAll(".negativeButton").forEach(a => a.style.display = "none");
-    document.querySelectorAll(".neutralButton").forEach(a => a.style.display = "none");
-    document.querySelectorAll(".positiveButton").forEach(a => a.style.display = "none");
+    hideButtons();
+    reportAcudits.push(report);  // we add the report object to the report
     console.table(reportAcudits);
 }
+
+getMeteo();
